7b8276f1badbda65aa1737381297a05bf9d3143d
Wagdy Samih <wagdysamih787>
2021-08-08 22:46:00 +0200

    Added Home Page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import SearchBar from "../components/SearchBar/SearchBar";
-import Note from "../components/note/Note";
 import AddNote from "../components/AddNote/AddNote";
+import NotesList from "../components/NotesList/NotesList";
+
+import { useState, useEffect } from "react";
+let { getNotes } = require("../Services/Notes.service");
 
 function Home() {
+  let resData;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(async () => {
+    resData = await getNotes();
+    if (!notes.length && resData) {
+      setNotes(resData);
+    }
+  }, []);
+
   return (
     <div>
-      <SearchBar />
-      <AddNote />
-      <Note />
+      <SearchBar setNotes={setNotes}/>
+      <AddNote setNotes={setNotes} />
+      <NotesList notes={notes} setNotes={setNotes} />
     </div>
   );
 }
